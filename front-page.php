4b2edfb2c2e5c386f04674215198f2d98c63b519
Ayumi Kojima <ayumikjm@gmail.com>
2017-05-18 15:06:13 -0700

    Static home page

--- front-page.php
+++ front-page.php
@@ -7,21 +7,34 @@
         <div class="flexslider">
           <ul class="slides">
             <li>
-              <img src="<?php bloginfo('template_directory'); ?>/images/slider3.jpg"/>
+              <img src="http://www.ayumik.com/wordpress/images/slider3.jpg"/>
             </li>
             <li>
-              <img src="<?php bloginfo('template_directory'); ?>/images/slider2.jpg" />
+              <img src="http://www.ayumik.com/wordpress/images/slider2.jpg" />
             </li>
             <li>
-              <img src="<?php bloginfo('template_directory'); ?>/images/main.jpg" />
+              <img src="http://www.ayumik.com/wordpress/images/main.jpg" />
             </li>
             <li>
-              <img src="<?php bloginfo('template_directory'); ?>/images/slider4.jpg" />
+              <img src="http://www.ayumik.com/wordpress/images/slider4.jpg" />
             </li>
           </ul>
         </div>
       
 <div id="content">
+    
+<?php if ( have_posts() ) : while( have_posts() ) : the_post(); // start loop one ?>
+<?php the_content(''); // get the home page's content ?>
+<?php endwhile; endif; // end loop one ?>
+    
+    
+<?php rewind_posts(); // stop loop one ?>
+<?php query_posts('showposts=4'); // give directions to loop two ?>
+<?php while (have_posts()) : the_post(); // start loop two ?>
+<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+<?php endwhile; // end loop two ?>
+    
+
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <h2><a href="<?php the_permalink();  ?>"><?php the_title(); ?></a></h2>
 <?php the_content(''); ?>
