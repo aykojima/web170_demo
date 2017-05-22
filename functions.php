025b97885603d793998bc04ec4fe89f29c960fce
Ayumi Kojima <ayumikjm@gmail.com>
2017-05-22 14:28:13 -0700

    Custom Posts Feed with Featured Image

--- functions.php
+++ functions.php
@@ -12,6 +12,9 @@ Version: 42.0
 register_nav_menus(array('main-menu' => __( 'Main Menu')));
 //
 
+//Add theme support for thumbnails for my posts
+//add_theme_support( 'post-thumbnails' );
 
-
+add_theme_support( 'post-thumbnails', array( 'post', 'page' ) );
+set_post_thumbnail_size('icon', 300, 300, true);
 ?>
--- index.php
+++ index.php
@@ -7,8 +7,18 @@
       
 <div id="content">
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<article class="post-excerpt">
 <h2><a href="<?php the_permalink();  ?>"><?php the_title(); ?></a></h2>
-<?php the_content(''); ?>
+<small>
+    Posted on <?php the_time('F j, Y'); ?> 
+    by <?php the_author(); ?> 
+    in <?php the_category(', '); ?>
+</small>
+    <a href=" <?php the_permalink(); ?>"><?php the_post_thumbnail( 'thumbnail' ); ?> </a>
+<?php the_excerpt(); ?>
+<p id="read-more"><a href="<?php the_permalink(); ?>">Read More</a></p>    
+</article>
+
 <?php endwhile; endif;  ?>
 <?php if(is_404()){ echo 'Nothing found.'; }?>
     
